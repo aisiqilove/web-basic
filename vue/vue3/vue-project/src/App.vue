58cabf310e1875fc8e1ff9f4a75abcc62d7327aa
maxzzli <592941317@qq.com>
2023-07-20 16:31:30 +0800

    feat: 完善滚动

---
+++ vue/vue3/vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { effect } from "vue";
+import InfiniteList from "./components/InfiniteList.vue";
+
+const songs: Array<any> = []; // 列表数据
+const initData = () => {
+  for (let index = 0; index < 1000; index++) {
+    const element = index;
+    songs.push({
+      title: `歌曲${element}`,
+      idx: index
+    });
+  }
+};
+
+effect(() => {
+  initData();
+});
+</script>
+
+<template>
+  <infinite-list :listData="songs">
+    <template #default="{ listItem }">
+      <div>{{ listItem.title }}</div>
+      <!-- ... -->
+    </template>
+  </infinite-list>
+</template>
